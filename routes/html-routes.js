2a5681c6fd53321805b103288828e816461d3abc
Leyla Kaplan <leylakaplan@leylas-mbp.lan>
2020-02-04 16:53:21 -0800

    more routes added

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -15,4 +15,12 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
+  app.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+
+  app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+
 };
